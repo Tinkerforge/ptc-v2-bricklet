d0c830bb031430361eb7f8f04792dfebf52fa861
Erik Fleckstein <erik@tinkerforge.com>
2020-08-25 15:54:13 +0200

    uc: Fix whitespace

--- software/examples/uc/example_callback.c
+++ software/examples/uc/example_callback.c
@@ -1,5 +1,3 @@
-
-
 #include "bindings/hal_common.h"
 #include "bindings/bricklet_ptc_v2.h"
 
@@ -7,15 +5,12 @@
 
 void check(int rc, const char* msg);
 
-
-
 // Callback function for temperature callback
 void temperature_handler(TF_PTCV2 *device, int32_t temperature, void *user_data) {
 	(void)device; (void)user_data; // avoid unused parameter warning
 
 	tf_hal_printf("Temperature: %d 1/%d °C\n", temperature, 100.0);
 }
-
 
 TF_PTCV2 ptc;
 
--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -1,14 +1,9 @@
-
-
 #include "bindings/hal_common.h"
 #include "bindings/bricklet_ptc_v2.h"
 
 #define UID "XYZ" // Change XYZ to the UID of your PTC Bricklet 2.0
 
 void check(int rc, const char* msg);
-
-
-
 
 TF_PTCV2 ptc;
 
