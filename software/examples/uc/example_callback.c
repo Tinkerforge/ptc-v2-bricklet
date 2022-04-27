e4739a9b401f778f6412eda4cf46b25686528e16
Matthias Bolte <matthias@tinkerforge.com>
2022-04-27 15:37:18 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_callback.c
+++ software/examples/uc/example_callback.c
@@ -1,17 +1,13 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_ptc_v2.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_ptc_v2.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your PTC Bricklet 2.0
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 // Callback function for temperature callback
 static void temperature_handler(TF_PTCV2 *device, int32_t temperature, void *user_data) {
@@ -24,7 +20,7 @@ static TF_PTCV2 ptc;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_ptc_v2_create(&ptc, UID, hal), "create device object");
+	check(tf_ptc_v2_create(&ptc, NULL, hal), "create device object");
 
 	// Register temperature callback to function temperature_handler
 	tf_ptc_v2_register_temperature_callback(&ptc,
